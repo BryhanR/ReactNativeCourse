0bea193fafed92c0e052b3724dfd4e9b546ad7a2
bryhan <brarodriguezm@hotmail.com>
2018-05-16 23:55:03 -0600

    fixed album list issue

--- album/src/components/AlbumList.js
+++ album/src/components/AlbumList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Alert } from 'react-native';
 
 import axios from 'axios';
-import AlbumDetail from './AlbumDetail'
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
@@ -11,12 +11,21 @@ class AlbumList extends Component {
   };
 
   componentWillMount() {
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
+        { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ],
+      { cancelable: false }
+    );
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(r => this.setState({ albums: r.data }));
   }
 
   renderAlbums() {
-    console.log('Estos son los albums');
     return this.state.albums.map(a =>
       <AlbumDetail key={a.title} album={a} />
     );
@@ -24,7 +33,7 @@ class AlbumList extends Component {
 
   render() {
     return (
-        <View style="viewStyle">
+        <View>
           {this.renderAlbums()}
         </View>
     );
